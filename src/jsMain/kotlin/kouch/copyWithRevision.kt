73fc41595c6718adedf9ded0b349ac2066a4be0d
Lev Shagalov <lllewik@gmail.com>
2020-09-27 14:44:00 +0300

    try enable js

--- src/jsMain/kotlin/kouch/copyWithRevision.kt
+++ src/jsMain/kotlin/kouch/copyWithRevision.kt
@@ -1,12 +1,11 @@
 package kouch
 
 actual inline fun <reified T : KouchEntity> T.copyWithRevision(revision: String): T {
-    println(this)
-    println(this::class)
+    TODO()
 //    val copy = this::class.members.first { it.name == "copy" }
 //    val instanceParam = copy.parameters.first { it.kind == KParameter.Kind.INSTANCE }
 //    val revisionParam = copy.parameters.first { it.name == "revision" }
 //    @Suppress("UNCHECKED_CAST")
 //    return copy.callBy(mapOf(instanceParam to this, revisionParam to revision)) as T
-    return this
+
 }
--- src/jsTest/kotlin/kouch/runTest.kt
+++ src/jsTest/kotlin/kouch/runTest.kt
@@ -3,5 +3,5 @@ package kouch
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 
-actual fun runTest(block: suspend () -> Unit): dynamic = GlobalScope.promise { block() }
-actual fun <T> runTestWithResult(block: suspend () -> T): dynamic = GlobalScope.promise { block() }
+actual fun runTest(block: suspend () -> Unit): dynamic = TODO()//GlobalScope.promise { block() }
+actual fun <T> runTestWithResult(block: suspend () -> T): dynamic = TODO()//GlobalScope.promise { block() }
