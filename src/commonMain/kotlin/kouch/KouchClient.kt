5995a568469a756b6b654d0ec245db08110fb245
Lev Shagalov <lllewik@gmail.com>
2020-09-29 20:55:23 +0300

    getMetadata function 0.0.16

--- src/commonMain/kotlin/kouch/KouchClient.kt
+++ src/commonMain/kotlin/kouch/KouchClient.kt
@@ -4,10 +4,12 @@ import kouch.client.KouchDatabaseService
 import kouch.client.KouchDesignService
 import kouch.client.KouchDocumentService
 import kouch.client.KouchServerService
+import kotlin.reflect.KClass
 
 abstract class KouchClient {
     abstract val server: KouchServerService
     abstract val db: KouchDatabaseService
     abstract val doc: KouchDocumentService
     abstract val design: KouchDesignService
+    abstract fun <T : KouchEntity> getMetadataFor(kClass: KClass<T>): KouchMetadata
 }
--- src/commonMain/kotlin/kouch/client/KouchClientImpl.kt
+++ src/commonMain/kotlin/kouch/client/KouchClientImpl.kt
@@ -2,6 +2,9 @@ package kouch.client
 
 import kouch.Context
 import kouch.KouchClient
+import kouch.KouchEntity
+import kouch.getMetadata
+import kotlin.reflect.KClass
 
 class KouchClientImpl(
     val context: Context
@@ -10,4 +13,6 @@ class KouchClientImpl(
     override val db = KouchDatabaseService(context)
     override val doc = KouchDocumentService(context, KouchDocumentService.Type.DOC)
     override val design = KouchDesignService(context, KouchDocumentService(context, KouchDocumentService.Type.DESIGN))
+
+    override fun <T : KouchEntity> getMetadataFor(kClass: KClass<T>) = context.getMetadata(kClass)
 }
