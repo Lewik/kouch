be6b0c348f03d18505b85b5e7f2357729ff91f65
Lev Shagalov <lllewik@gmail.com>
2020-09-30 20:58:37 +0300

    ktor market as api 0.0.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("reflect"))
-                implementation("io.ktor:ktor-client-core:$KTOR_VERSION")
+                api("io.ktor:ktor-client-core:$KTOR_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$KOTLINX_COROUTINES_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
@@ -62,7 +62,7 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-cio:$KTOR_VERSION")
+                api("io.ktor:ktor-client-cio:$KTOR_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$KOTLINX_COROUTINES_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
@@ -75,7 +75,7 @@ kotlin {
         }
         val jsMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-js:$KTOR_VERSION")
+                api("io.ktor:ktor-client-js:$KTOR_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$KOTLINX_COROUTINES_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
