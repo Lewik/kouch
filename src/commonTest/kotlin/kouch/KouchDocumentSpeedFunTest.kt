71c24d1068f3cfb69c5a5488860c1a4aaa97d9af
nullsanya <000sanya.000sanya@gmail.com>
2020-12-29 22:34:46 +1000

    add speed test for erlang and js views

--- src/commonTest/kotlin/kouch/KouchDocumentSpeedFunTest.kt
+++ src/commonTest/kotlin/kouch/KouchDocumentSpeedFunTest.kt
@@ -1,6 +1,7 @@
 package kouch.kouch
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kouch.*
@@ -9,6 +10,7 @@ import kotlin.random.Random
 import kotlin.test.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
+import kotlin.time.seconds
 
 internal class KouchDocumentSpeedFunTest {
     @KouchEntityMetadata("test_entity", "test_entity")
@@ -96,38 +98,27 @@ internal class KouchDocumentSpeedFunTest {
         label = randomString(24),
     )
 
+    @Ignore
+    @Test
+    fun testManyUpsertsJs() = runTest {
+        testManyUpserts {
+            getJsDesignDoc(it)
+        }
+    }
 
     @Ignore
     @Test
-    fun testManyUpserts() = runTest {
+    fun testManyUpsertsErlang() = runTest {
+        testManyUpserts {
+            getErlangDesignDoc(it)
+        }
+    }
+
+    private suspend fun testManyUpserts(designCallback: (Int) -> KouchDesign) {
         kouch.db.createForEntity(randomEntity())
 
         (0..10).forEach {
-            val prefix = if (it == 0) "" else it.toString()
-            val design = KouchDesign(
-                id = "testdes$it",
-                views = mapOf(
-                    "all${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { emit(doc.label${prefix}, doc) }"""
-                    ),
-                    "allnull${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { emit(doc.label${prefix}, null) }"""
-                    ),
-                    "by_label${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { if (doc.label${prefix} != null) emit(doc.label${prefix}, doc) }"""
-                    ),
-                    "asd_only${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { if (doc.label${prefix} === "ASD") { emit(doc.label${prefix}, doc); } }"""
-                    ),
-                    "by_label_and_string${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { emit([doc.label${prefix}, doc.string], doc) }"""
-                    ),
-                    "reduce${prefix}" to KouchDesign.View(
-                        /*language=js*/ map = """doc => { emit(doc.label${prefix}, doc) }""",
-                        /*language=js*/ reduce = """(key, value) => { return true }"""
-                    )
-                )
-            )
+            val design = designCallback(it)
             val updateResult = kouch.design
                 .upsert(
                     ddoc = design,
@@ -139,11 +130,123 @@ internal class KouchDocumentSpeedFunTest {
         }
 
 
-        val bulkSize = 10000
-        (0..1000).forEach { step ->
-            val entities = (0..bulkSize).map { randomEntity() }
-            val time = measureTime { kouch.db.bulkUpsert(entities) }
-            println("step $step, $time")
+        val time = measureTime {
+            val bulkSize = 10000
+            (0..100).forEach { step ->
+                while (kouch.server.activeTasks().isNotEmpty()) {
+                    println("Waiting...")
+                    delay(5.seconds)
+                }
+                val entities = (0..bulkSize).map { randomEntity() }
+                val time = measureTime { kouch.db.bulkUpsert(entities) }
+                println("step $step, $time")
+            }
         }
+        println("Time: ${time.inSeconds}")
+    }
+
+    private fun getJsDesignDoc(i: Int): KouchDesign {
+        val prefix = if (i == 0) "" else i.toString()
+        val design = KouchDesign(
+            id = "testdes$i",
+            views = mapOf(
+                "all${prefix}" to KouchDesign.View(
+                    /*language=js*/
+                    map = """doc => { emit(doc.label${prefix}, doc) }""",
+                ),
+                "allnull${prefix}" to KouchDesign.View(
+                    /*language=js*/
+                    map = """doc => { emit(doc.label${prefix}, null) }""",
+                ),
+                "by_label${prefix}" to KouchDesign.View(
+                    /*language=js*/ map = """doc => { if (doc.label${prefix} != null) emit(doc.label${prefix}, doc) }"""
+                ),
+                "asd_only${prefix}" to KouchDesign.View(
+                    /*language=js*/ map = """doc => { if (doc.label${prefix} === "ASD") { emit(doc.label${prefix}, doc); } }"""
+                ),
+                "by_label_and_string${prefix}" to KouchDesign.View(
+                    /*language=js*/ map = """doc => { emit([doc.label${prefix}, doc.string], doc) }"""
+                ),
+                "reduce${prefix}" to KouchDesign.View(
+                    /*language=js*/ map = """doc => { emit(doc.label${prefix}, doc) }""",
+                    /*language=js*/ reduce = """(key, value) => { return true }"""
+                )
+            )
+        )
+        return design
+    }
+
+    private fun getErlangDesignDoc(i: Int): KouchDesign {
+        val prefix = if (i == 0) "" else i.toString()
+        val design = KouchDesign(
+            id = "testdes$i",
+            language = "Erlang",
+            views = mapOf(
+                "all${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/
+                    map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    Emit(K, {Doc})
+                                end.
+                            """.trimIndent(),
+                ),
+                "allnull${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/
+                    map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    Emit(K, null)
+                                end.
+                            """.trimIndent(),
+                ),
+                "by_label${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/
+                    map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    if
+                                      K == null -> null;
+                                      true -> Emit(null, null)
+                                    end
+                                end.
+                            """.trimIndent(),
+                ),
+                "asd_only${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/ map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    if
+                                      K == <<"ASD">> -> Emit(K, {Doc});
+                                      true -> null
+                                    end
+                                end.
+                            """.trimIndent()
+                ),
+                "by_label_and_string${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/ map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    S = proplists:get_value(<<"string">>, Doc, null),
+                                    Emit([K, S], {Doc})
+                                end.
+                            """.trimIndent()
+                ),
+                "reduce${prefix}" to KouchDesign.View(
+                    /*language=Erlang*/ map = """
+                                fun({Doc}) ->
+                                    K = proplists:get_value(<<"label${prefix}">>, Doc, null),
+                                    Emit(K, {Doc})
+                                end.
+                            """.trimIndent(),
+                    /*language=Erlang*/ reduce = """
+                                fun(Keys, Values, ReReduce) -> 
+                                  true
+                                end.
+                            """.trimIndent()
+                )
+            )
+        )
+        return design
     }
 }
