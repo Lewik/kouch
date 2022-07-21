0d55edd166bc0a1363d37941a47c6c6d78ccd4eb
Lev Shagalov <lllewik@gmail.com>
2022-07-21 20:17:01 +0300

    version downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `maven-publish`
 }
 group = "kouch"
-version = "0.0.52"
+version = "0.0.53"
 
 repositories {
     mavenCentral()
--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,6 +1,6 @@
 import org.gradle.api.JavaVersion
 
-const val KOTLIN_VERSION = "1.7.10"
+const val KOTLIN_VERSION = "1.6.20"
 const val KOTLINX_COROUTINES_VERSION = "1.6.4"
 const val SERIALIZATION_VERSION = "1.4.0-RC"
 const val KTOR_VERSION = "2.0.3"
